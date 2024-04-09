d9601c2492d2812ed67d7896e75f66ae7c225b8c
Nick Balatos <nbalatos@gmail.com>
2024-04-09 14:02:14 +0300

    reverted old changes #4

--- pages/clustering.py
+++
@@ -1,5 +0,0 @@
-from st_pages import show_pages_from_config, add_page_title
-
-# Either this or add_indentation() MUST be called on each page in your
-# app to add indendation in the sidebar
-add_page_title()
